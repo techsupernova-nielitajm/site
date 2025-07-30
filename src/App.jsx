4af4eda2e3823bd613c8a99416bd996b94f03a6e
Jigyasu <techsupernova.nielitajm@gmail.com>
2025-07-30 12:36:20 +0530

    hh

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from './components/footer'
 import Gallery from './components/galary.jsx'
 import Home from './components/home.jsx'
 import Club from './components/club.jsx'
-import { BrowserRouter, Routes, Route } from "react-router";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import CookiePolicy from './components/cookie.jsx';
 import PrivacyPolicy from './components/privacy.jsx';
 import TermsOfService from './components/tos.jsx';
@@ -20,7 +20,7 @@ function App() {
   return (
     <>
     <div className="min-h-[100vh]   bg-repeat bg-stone-900  backdrop-blur-xl  linear-gradient(to_right,transparent_0%,black_5%,black_95%,transparent_100%)">
-      <BrowserRouter>
+   <HashRouter>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
@@ -32,7 +32,7 @@ function App() {
           <Route path="/leaderboard" element={<Leader />} />
         </Routes>
         <Footer />
-      </BrowserRouter>
+      </HashRouter>
     </div>
     </>
   )
