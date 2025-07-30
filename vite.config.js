934a8d5ff6ae7c34bf47d49e5e1432d583812fa5
Jigyasu <techsupernova.nielitajm@gmail.com>
2025-07-30 11:56:23 +0530

    hh

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
-import path from 'path';
+ 
 
 // https://vite.dev/config/
 export default defineConfig({
